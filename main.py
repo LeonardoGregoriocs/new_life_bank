ed498f0893ebfb86d152cd36c426e44ee3cc1dac
LeonardoGregoriocs <Leonardogregoriocs@gmail.com>
2021-10-11 20:37:54 -0300

    criando a port 8003/ criando get

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from datetime import datetime
+from datetime import date, datetime
 from typing import Dict
 from fastapi import FastAPI
+import uvicorn
+
 
 app = FastAPI()
 
-@app.get("/ping")
+@app.get("/")
 def root() -> str:
-    return "Pong"
+    return "Welcome to New Life Bank!"
+
+@app.get("/health")
+def alive () -> Dict[str, datetime]:
+    return {"timestamp" : datetime.now()}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8003, reload=True)
